6f900ccae814210533dbc7576a7baaa2c19c6f1e
ArponAG <34260294+ArponAG@users.noreply.github.com>
2022-08-23 15:42:28 +0600

    Update AnimeAdventures.lua

--- AnimeAdventures.lua
+++ AnimeAdventures.lua
@@ -1,5 +1,5 @@
 -- v1.4.6 --
--- + Fixed Auto Ability
+-- + Fixed Auto Ability [if it breaks dm Arpon AG#6612]
 
 -- v1.4.5 --
 -- + Added Hollow World
@@ -936,7 +936,6 @@ coroutine.resume(coroutine.create(function()
                 if game.PlaceId ~= 8304191830 then
                     repeat task.wait() until game:GetService("Workspace"):WaitForChild("_UNITS")
                     for i, v in ipairs(game:GetService("Workspace")["_UNITS"]:GetChildren()) do
-                        repeat task.wait() until v:WaitForChild("_stats")			
                        if v:FindFirstChild("_stats") then
                             if tostring(v["_stats"].player.Value) == game.Players.LocalPlayer.Name and v["_stats"].xp.Value > 0 then
                                 game:GetService("ReplicatedStorage").endpoints.client_to_server.use_active_attack:InvokeServer(v)
