c94bd62fc08a87c0050f82cbe82cb0b01fe44843
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-04-01 09:44:50 +0700

    Update AnimeAdventures.lua

--- AnimeAdventures.lua
+++ AnimeAdventures.lua
@@ -2583,14 +2583,14 @@ coroutine.resume(coroutine.create(function()
                 pcall(function() webhook() end)
                 print("Wait next or leave")
                 task.wait(2.1)
-                if Settings.AutoReplay then
-                    local a={[1]="replay"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
-                    local a={[1]="replay"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
-                    print("Replay...")        
-                elseif Settings.AutoNext then
+                if Settings.AutoNext then
                     local a={[1]="next_story"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
                     local a={[1]="next_story"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
                     print("Next Story...") 
+                elseif Settings.AutoReplay then
+                    local a={[1]="replay"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+                    local a={[1]="replay"} game:GetService("ReplicatedStorage").endpoints.client_to_server.set_game_finished_vote:InvokeServer(unpack(a))
+                    print("Replay...") 
                 elseif Settings.AutoContinue then
                     local a={[1]="NextRetry"} game:GetService("ReplicatedStorage").endpoints.client_to_server.request_start_infinite_tower_from_game:InvokeServer(unpack(a))
                     local a={[1]="NextRetry"} game:GetService("ReplicatedStorage").endpoints.client_to_server.request_start_infinite_tower_from_game:InvokeServer(unpack(a))   
