ec073bcfe6613915c2be5afcfa50205b384d6f6e
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-04-11 15:54:57 +0700

    Update AnimeAdventures.lua

--- AnimeAdventures.lua
+++ AnimeAdventures.lua
@@ -3951,11 +3951,11 @@ if game.PlaceId ~= 8304191830 then
     game.Players.LocalPlayer.PlayerGui.MessageGui.Enabled = false --disables the annoying error messages 
 end
 --disms
-if game.PlaceId == 8304191830 then
+--[[if game.PlaceId == 8304191830 then
     game:GetService("ReplicatedStorage").packages.assets["ui_sfx"].error.Volume = 0
     game:GetService("ReplicatedStorage").packages.assets["ui_sfx"].error_old.Volume = 0
     game.Players.LocalPlayer.PlayerGui.MessageGui.Enabled = false --disables the annoying error messages 
-end
+end]]
 
 pcall(function()
     local vu = game:GetService("VirtualUser")
