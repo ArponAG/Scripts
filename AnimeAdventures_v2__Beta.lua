d1ad5ee4499989941c9d86b0c9c097307fba3b80
siradaniy <83637146+siradaniy@users.noreply.github.com>
2023-08-06 03:22:16 +0700

    Update AnimeAdventures_v2__Beta.lua

--- AnimeAdventures_v2__Beta.lua
+++ AnimeAdventures_v2__Beta.lua
@@ -3647,8 +3647,6 @@ function autoload()
         if exec == "Synapse X" and Settings.AutoLoadScript then
             syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ArponAG/Scripts/main/AnimeAdventures_v2__Beta.lua'))()")
         elseif exec ~= "Synapse X" and Settings.AutoLoadScript then
-            queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ArponAG/Scripts/main/AnimeAdventures_v2__Beta.lua'))()")
-        elseif exec ~= "Synapse X" and Settings.AutoLoadScript then
             syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ArponAG/Scripts/main/AnimeAdventures_v2__Beta.lua'))()")
         end
     end)
